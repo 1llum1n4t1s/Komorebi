6f5ef4907b5fb2b84bdd92d14dd02dc9e6c665c4
leo <longshuang@msn.cn>
2026-03-09 16:28:19 +0800

    enhance: auto-focus the first input element in popup (#2172)
    Signed-off-by: leo <longshuang@msn.cn>

--- src/Views/PopupDataTemplates.cs
+++ src/Views/PopupDataTemplates.cs
@@ -1,20 +1,43 @@
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Controls.Templates;
+using Avalonia.Input;
+using Avalonia.VisualTree;
 
-namespace Komorebi.Views
+namespace SourceGit.Views
 {
-    /// <summary>
-    ///     ポップアップダイアログのViewModel→Viewマッピングを管理するデータテンプレートクラス。
-    /// </summary>
     public class PopupDataTemplates : IDataTemplate
     {
-        /// <summary>
-        ///     Buildの処理を行う。
-        /// </summary>
-        public Control Build(object param) => App.CreateViewForViewModel(param);
-        /// <summary>
-        ///     Matchの処理を行う。
-        /// </summary>
-        public bool Match(object data) => data is ViewModels.Popup;
+        public bool Match(object data)
+        {
+            return data is ViewModels.Popup;
+        }
+
+        public Control Build(object param)
+        {
+            var control = App.CreateViewForViewModel(param);
+
+            control.Loaded += (o, e) =>
+            {
+                if (o is not Control ctl)
+                    return;
+
+                var inputs = ctl.GetVisualDescendants();
+                foreach (var input in inputs)
+                {
+                    if (input is SelectableTextBlock)
+                        continue;
+
+                    if (input is InputElement { Focusable: true, IsEffectivelyEnabled: true } focusable)
+                    {
+                        focusable.Focus(NavigationMethod.Directional);
+                        if (input is TextBox box)
+                            box.CaretIndex = box.CaretIndex = box.Text?.Length ?? 0;
+                        return;
+                    }
+                }
+            };
+
+            return control;
+        }
     }
 }
