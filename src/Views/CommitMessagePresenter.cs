6492d8e8aa95a7463700dc3d8db2117909a6bb9f
leo <longshuang@msn.cn>
2026-04-07 10:53:39 +0800

    enhance: use `string.Equals` instead of operator `==`
    Signed-off-by: leo <longshuang@msn.cn>
    (cherry picked from commit f8b91ecaca2fbcd5c54527c89d6670b78033cd4e)

--- src/Views/CommitMessagePresenter.cs
+++ src/Views/CommitMessagePresenter.cs
@@ -286,7 +286,7 @@ public class CommitMessagePresenter : SelectableTextBlock
                 {
                     var currentParent = this.FindAncestorOfType<CommitBaseInfo>();
                     if (currentParent is { DataContext: ViewModels.CommitDetail currentDetail } &&
-                        currentDetail.Commit.SHA == lastDetailCommit)
+                        currentDetail.Commit.SHA.Equals(lastDetailCommit, StringComparison.Ordinal))
                     {
                         _inlineCommits.TryAdd(sha, c);
                         if (_lastHover == link && c is not null)
